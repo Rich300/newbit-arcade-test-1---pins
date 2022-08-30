b8ff330ebfef80f58d4e5f1efa016d180fad4d8d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-30 20:56:35 +0100

    Restore '30/08/2022, 20:41:55 Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    colorbit_51bit.clear()
-    colorbit_51bit.show()
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    colorbit_51bit.showColor(colorbit.colors(BitColors.Red))
-    colorbit_51bit.show()
-})
-let colorbit_51bit: colorbit.Strip = null
-let PinNo = 2
 let mySprite = sprites.create(img`
     .............ccfff..............
     ...........ccddbcf..............
@@ -27,10 +17,6 @@ let mySprite = sprites.create(img`
     .............fffff..............
     `, SpriteKind.Player)
 mySprite.setStayInScreen(true)
-colorbit_51bit = colorbit.initColorBit(PinNo, BitColorMode.RGB)
-colorbit_51bit.setBrightness(32)
-colorbit_51bit.clear()
-colorbit_51bit.show()
 game.onUpdate(function () {
     controller.moveSprite(mySprite)
 })
