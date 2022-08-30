932fc95e926b91f9fc6bacd4e608c35d9ad8a7f0
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-30 20:54:01 +0100

    see is buttons & pins work

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    colorbit_51bit.clear()
+    colorbit_51bit.show()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    colorbit_51bit.showColor(colorbit.colors(BitColors.Red))
+    colorbit_51bit.show()
+})
+let colorbit_51bit: colorbit.Strip = null
+let PinNo = 2
 let mySprite = sprites.create(img`
     .............ccfff..............
     ...........ccddbcf..............
@@ -17,6 +27,10 @@ let mySprite = sprites.create(img`
     .............fffff..............
     `, SpriteKind.Player)
 mySprite.setStayInScreen(true)
+colorbit_51bit = colorbit.initColorBit(PinNo, BitColorMode.RGB)
+colorbit_51bit.setBrightness(32)
+colorbit_51bit.clear()
+colorbit_51bit.show()
 game.onUpdate(function () {
     controller.moveSprite(mySprite)
 })
